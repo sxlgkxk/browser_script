cd867d1c8c5db7d4ec5c9b4553fa040561898313
sxlgkxk <sxlgkxk@gmail.com>
2022-02-11 19:00:29 +0800

    daily update

--- zhihu_analysis.js
+++ zhihu_analysis.js
@@ -1,3 +1,19 @@
-console.log("version2")
-console.log("version2")
-alert("v2")
+// ==UserScript==
+// @name         zhihu analysis
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author 		 sxlgkxk
+// @include      https://www.zhihu.com/people/*
+// @icon         https://www.google.com/s2/favicons?domain=38.53
+// @grant        none
+// ==/UserScript==
+
+(function() {
+
+	console.log("version2")
+	console.log("version2")
+	alert("v2")
+
+
+})();
