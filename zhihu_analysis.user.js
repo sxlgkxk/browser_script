86af75353442bbbda24f66c8ee595baaac9c88a5
sxlgkxk <sxlgkxk@gmail.com>
2022-02-14 19:57:24 +0800

    daily update

--- zhihu_analysis.user.js
+++ zhihu_analysis.user.js
@@ -1,21 +1,97 @@
 // ==UserScript==
-// @name        zhihu analysis
-// @version     0.1
-// @description analyse zhihu users
-// @license     MIT
-// @author      sxlgkxk
-// @namespace   https://github.com/sxlgkxk
-// @include     https://www.zhihu.com/people/*
-// @run-at      document-idle
-// @icon        https://github.githubassets.com/pinned-octocat.svg
-// @updateURL   https://github.com/sxlgkxk/browser_script/raw/main/zhihu_analysis.user.js
-// @downloadURL https://github.com/sxlgkxk/browser_script/raw/main/zhihu_analysis.user.js
-// @supportURL  https://github.com/sxlgkxk/browser_script/issues
+// @name         zhihu analysis
+// @include      https://www.zhihu.com/people/*
+// @updateURL    https://github.com/sxlgkxk/browser_script/raw/main/zhihu_analysis.user.js
+// @downloadURL  https://github.com/sxlgkxk/browser_script/raw/main/zhihu_analysis.user.js
+// @supportURL   https://github.com/sxlgkxk/browser_script/issues
+// @version      0.1
+// @description  try to take over the world!
+
+// @namespace    http://sxlgkxk.github.io/
+// @author       sxlgkxk
+// @icon         http://sxlgkxk.github.io/im/avatar.jpg
+// @license      MIT
+
+// @require      https://cdn.jsdelivr.net/npm/vue@3/dist/vue.global.prod.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
+
+let content=''
+let itemlist=[]
+let body=null
+let app=null
+let internal=null
+
+
+// answerTest
+function answerTest(){
+	itemlist=document.querySelectorAll('a[data-za-detail-view-element_name="Title"]')
+
+	// answer ready
+	if (itemlist.length > 0){
+		for(let i=0;i<itemlist.length;i++){
+			content+='<li>'+itemlist[i].outerHTML+'</li>'
+		}
+		content+='</br>'
+		app.innerHTML=content
+
+		// into next page
+		document.querySelector('#ProfileMain > div.ProfileMain-header > ul > li:nth-child(9) > a').click()
+
+		// start next interval
+		clearInterval(interval)
+		interval=setInterval(followTest, 200)
+	}
+}
+
+// followTest
+function followTest(){
+	itemlist=document.querySelectorAll('.UserItem-title a[data-za-detail-view-element_name="User"]')
+
+	// answer ready
+	if (itemlist.length > 0){
+
+		content+=`<table style="width:100%">`
+
+		for(let i=0;i<itemlist.length;i++){
+			let user=itemlist[i].outerHTML
+			let desc=document.querySelector('#Profile-following > div:nth-child(2) > div:nth-child('+(i+1)+') > div > div > div.ContentItem-head > div > div > div.ztext')
+			if (desc) desc=desc.innerHTML
+			content+='<tr><td style="text-align: right; padding-right:20px">'+user+'</td><td>'+desc+'</td></tr>'
+		}
+		content+='</table>'
+		app.innerHTML=content
+
+		// into next page
+
+		// start next interval
+		clearInterval(interval)
+		// interval=setInterval(followTest, 200)
+	}
+}
+
 (function() {
+	// into answer page
+	document.querySelector('#ProfileMain > div.ProfileMain-header > ul > li:nth-child(2) > a').click()
 
-	console.log("version2")
-	console.log("version2")
+	setTimeout(()=>{
+		// version
+		console.log("version5")
 
+		// dom
+		body=document.querySelector('#ProfileHeader > div')
+		app=document.createElement('div')
+		body.before(app)
 
+		// style
+		app.style.lineHeight='2'
+		app.style.color='#c6c6c6'
+		app.style.backgroundColor='#333'
+		app.style.padding='20px'
+
+		interval=setInterval(answerTest, 200)
+
+	}, 4000)
 })();
