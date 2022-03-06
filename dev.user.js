74073b3c5bcbacc5bf59416c153050de8232e2db
sxlgkxk <sxlgkxk@gmail.com>
2022-03-06 13:28:09 +0800

    pc update

---
+++ dev.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         dev
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @grant        none
+
+// @include      http://readonlinefreebook.com/*
+// @include      https://readonlinefreebook.com/*
+// @require      file://D:\storage\repo\github\sxlgkxk\public\webapp\browser_script\read_mark.user.js
+
+// @include1      https://dida365.com/webapp/*
+// @require1      file://D:\storage\repo\github\sxlgkxk\public\webapp\browser_script\ticktick_gtd.user.js
+
+// @include1      https://dida365.com/webapp/*
+// @require1      file://D:\storage\repo\github\sxlgkxk\public\webapp\browser_script\ticktick_gtd.user.js
+
+// @include1      https://github.com*
+// @require1      file://D:\storage\repo\github\sxlgkxk\public\webapp\browser_script\github.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+})();
