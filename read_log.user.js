1c09d91db3964e0a42696ba4a95150627754672f
sxlgkxk <sxlgkxk@gmail.com>
2022-03-18 15:13:15 +0800

    update

--- read_log.user.js
+++ read_log.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         read_record
+// @name         read_log
 // @include      http://readonlinefreebook.com/*
 // @include      https://readonlinefreebook.com/*
-// @updateURL    https://github.com/sxlgkxk/browser_script/raw/main/read_record.user.js
-// @downloadURL  https://github.com/sxlgkxk/browser_script/raw/main/read_record.user.js
+// @updateURL    https://github.com/sxlgkxk/browser_script/raw/main/read_log.user.js
+// @downloadURL  https://github.com/sxlgkxk/browser_script/raw/main/read_log.user.js
 // @supportURL   https://github.com/sxlgkxk/browser_script/issues
 // @version      0.1
-// @description  read record
+// @description  read log
 // @namespace    http://sxlgkxk.github.io/
 // @author       sxlgkxk
 // @icon         http://sxlgkxk.github.io/im/avatar.jpg
@@ -16,7 +16,43 @@
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
+//-------------------------------- common functions --------------------------------
+
+function getScrollPercent() {
+    var h = document.documentElement,
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return ((h[st]||b[st]) / ((h[sh]||b[sh])) * 100).toFixed(2);
+}
+
 (function(){
+
+//-------------------------------- code snippets --------------------------------
+
+function addLine(){
+	line=document.createElement("hr")
+	line.classList.add("mark")
+	line.style.top=document.documentElement["scrollTop"]+100+"px"
+	line.style.left='0px'
+	line.style.width='100%'
+	body.before(line)
+}
+
+//-------------------------------- main --------------------------------
+
+// line style
+style=document.createElement("div")
+body.before(style)
+style.innerHTML =`<style>
+	hr.mark{
+		position: absolute;
+	}
+</style>`
+
+setInterval(()=>{
+	addLine()
+},1000*300)
 
 
 })();
