7265d2c4a58c64c9c4222b27976b693c2381f611
sxlgkxk <sxlgkxk@gmail.com>
2022-03-23 01:44:16 +0800

    pc update

--- module/three.js
+++ module/three.js
@@ -1,16 +1,16 @@
 (function(){
 
-class ThreeUtil{
-	static getDateStr(date=null){
-		date=date?date:new Date()
-		let year=String(date.getFullYear()).substring(2,4)
-		let month=String(date.getMonth()+1).padStart(2,'0')
-		let day=String(date.getDate()).padStart(2,'0')
-		return year+month+day
-	}
+class Three{
+	// Camera
+	// Object
+	// Map
+	// Screen
+	// Texture
+	// Music
+	// Control
 }
 
-window.ThreeUtil=ThreeUtil;
+window.Three=Three;
 
 
 
